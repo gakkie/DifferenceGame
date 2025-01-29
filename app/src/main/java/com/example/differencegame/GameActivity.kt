9786a724ecccfa6414095f2fe5e2b2120fed8bdd
gakkie <gakkie@mini>
2025-01-29 22:56:26 +0900

    ゲーム画面を仮実装：左右に画像を2つ並べるUIを配置

--- app/src/main/java/com/example/differencegame/GameActivity.kt
+++ app/src/main/java/com/example/differencegame/GameActivity.kt
@@ -1,20 +1,19 @@
 package com.example.differencegame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // もしルートレイアウト自体を操作したいなら、こんな感じで取得
+        val container = findViewById<ConstraintLayout>(R.id.gameContainer)
+
+        // 画像Viewを取得したい場合
+        val imageLeft = findViewById<android.widget.ImageView>(R.id.imageLeft)
+        val imageRight = findViewById<android.widget.ImageView>(R.id.imageRight)
     }
 }
